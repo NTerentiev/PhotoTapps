896d5b3bda5128561108088b166e71a22dede312
Nykyta <nikitaterentev8@gmail.com>
2022-10-15 14:14:38 +0300

    Add Photo

--- PhotoTapps/PhotosCollectionViewController.swift
+++ PhotoTapps/PhotosCollectionViewController.swift
@@ -8,27 +8,24 @@
 import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
+    let photos = ["dog1", "dog2", "dog3", "dog4", "dog5", "dog6", "dog7", "dog8", "dog9", "dog10", "dog11", "dog12", "dog13", "dog14", "dog15"]
     
-    let itemPerRow: CGFloat = 5
-    let sectionInserts = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+    let itemPerRow: CGFloat = 2
+    let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-//        layout.itemSize = CGSize(width: 70, height: 30)
-//        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        //        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        //        layout.itemSize = CGSize(width: 70, height: 70)
+        //        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        //        layout.minimumLineSpacing = 1
+        //        layout.minimumInteritemSpacing = 1
+        //        layout.scrollDirection = .vertical
+        //
+        //        collectionView.showsVerticalScrollIndicator = false
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: UICollectionViewDataSource
 
@@ -40,14 +37,17 @@ class PhotosCollectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 15
+        return photos.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath)
-    
-        cell.backgroundColor = .black
-    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
+        
+        let imageName = photos[indexPath.item]
+        let image = UIImage(named: imageName)
+        
+        cell.dogImageView.image = image
+
         return cell
     }
 
